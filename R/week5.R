cae66a2772be844811583148051fa4dfecbd07b5
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-18 15:12:17 -0600

    Edit 6 lines (typo) and try to finish data cleaning task 2 (line 18~21)

--- R/week5.R
+++ R/week5.R
@@ -5,7 +5,7 @@ library(tidyverse)
 # Data Import
 Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv("../data/Anotes.csv")
-Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum","parnum","stimver","datadate", paste0("q, 1:10")))
+Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum","parnum","stimver","datadate", paste0("q", 1:10)))
 Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
 
 # Data Cleaning
@@ -16,10 +16,9 @@ Aclean_tbl <- Adata_tbl %>%
   left_join(Anotes_tbl, by = "parnum") %>%
   filter(is.na(notes))
 
+Aclean_tbl
+
 ABclean_tbl <- Bdata_tbl %>%
   mutate(datadate = mdy_hms (datadate)) %>%
   mutate(across(q1:q10, as.integer)) %>%
-  left_join(Bnotes_tbl, by = "parnum") %>%
-  filter(is.na(notes))
-
-ABclean_tbl
+  left_join(Bnotes_tbl, by = "parnum") 
