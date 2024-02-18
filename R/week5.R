93ad37f31b50ce8ecc33de6b3516f5725cdd6e88
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-18 14:52:48 -0600

    Data import (Line 6~9)

--- R/week5.R
+++ R/week5.R
@@ -1,3 +1,9 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+# Data Import
+Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv("../data/Anotes.csv")
+Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum","parnum","stimver","datadate", paste0("q, 1:10")))
+Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
